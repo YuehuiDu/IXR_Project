40d1ac4287b08d288f7f159eeaa2a01e24ca5329
YuehuiDu <duyuehui97@gmail.com>
2023-11-07 21:44:02 -0500

    customized gesture added

--- IXR_Project/IXR_Project/Assets/Fraktalia/DreamStarGenerator/Scripts/DreamStarGenerator_Crazy.cs
+++ IXR_Project/IXR_Project/Assets/Fraktalia/DreamStarGenerator/Scripts/DreamStarGenerator_Crazy.cs
@@ -4,13 +4,20 @@ using System.Collections.Generic;
 
 namespace Fraktalia.DreamStarGen
 {
+    // public float angleSpeed = 1.0f; 
+    // private float currentAngle = 0.0f;
     public class DreamStarGenerator_Crazy : DreamStarGenerator
     {
 
 
 
         public override Vector3 StarAlgorithm(float Angle)
-        {
+        { 
+         
+        // currentAngle += angleSpeed * Time.deltaTime; 
+        // currentAngle = currentAngle % 360;
+        // Vector3 starPosition = StarAlgorithm(currentAngle);
+
             float r = 0;
 
             float radiant = Angle * a;
